c4afd67aa3f5e02f3beb6c149dc3e561f7580cf2
Fernando Brito <email@fernandobrito.com>
2016-08-05 09:58:51 -0300

    Oops again. Convert tabs to spaces

--- common.py
+++ common.py
@@ -2,13 +2,13 @@ import random
 
 # Remove first element from list and convert elements to integers
 def read_input(argv):
-	# Process arguments, removing first element (filename)
-	input = argv[1:]
+    # Process arguments, removing first element (filename)
+    input = argv[1:]
 
-	# Check size
-	if (len(input) == 0):
-		print("No input given. Using 10 random numbers")
-		input = random.sample(range(1, 100), 10)
+    # Check size
+    if (len(input) == 0):
+        print("No input given. Using 10 random numbers")
+        input = random.sample(range(1, 100), 10)
 
-	# Convert values from string to integer
-	return list(map(int, input))
+    # Convert values from string to integer
+    return list(map(int, input))
