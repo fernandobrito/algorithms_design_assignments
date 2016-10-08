89a38257c746a9bee60e156d71774529e7d71eec
Fernando Brito <email@fernandobrito.com>
2016-10-07 23:39:58 -0300

    Improve tests for coin_change

--- dynamic_greedy_lib/dynamic/tests/coin_change.py
+++ dynamic_greedy_lib/dynamic/tests/coin_change.py
@@ -3,4 +3,13 @@ from dynamic import coin_change
 
 runner = TestRunner(coin_change.solve)
 
+# Trivial examples
+runner.expect_equal([0, [1]], 1)
+runner.expect_equal([1, [1]], 1)
+runner.expect_equal([1, [2]], 0)
+runner.expect_equal([10, [2]], 1)
+runner.expect_equal([10, [2, 5]], 2)
+runner.expect_equal([15, [100]], 0)
+
+# Example from lecture
 runner.expect_equal([15, [2, 5, 10]], 3)
