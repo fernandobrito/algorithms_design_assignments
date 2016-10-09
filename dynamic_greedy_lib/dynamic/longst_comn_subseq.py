c9b92dec2229bba55fe92cb8885e448df589cfe9
Jorismar Barbosa <jorismar.barbosa@gmail.com>
2016-10-09 01:37:52 -0300

    Longest Common Subsequence added.

---
+++ dynamic_greedy_lib/dynamic/longst_comn_subseq.py
@@ -0,0 +1,49 @@
+from utils import table as table_utils
+
+def solve(input):
+    """
+    :param input: array with two strings
+    :return: length of longest common subsequence of the two strings
+    """
+
+    return _longest_common_subsequence(input[0], input[1])
+
+def _longest_common_subsequence(seq, sub):
+    seq_len = len(seq) + 1
+    sub_len = len(sub) + 1
+
+    table = table_utils.initialize(seq_len, sub_len, 0)
+
+    for r in range(1, seq_len):
+        r_bk = r - 1
+
+        for c in range(1, sub_len):
+            c_bk = c - 1
+
+            if(seq[r_bk] == sub[c_bk]):
+                table[r][c] = table[r_bk][c_bk] + 1
+            else:
+                r_val = table[r_bk][c]
+                c_val = table[r][c_bk]
+                table[r][c] = r_val if r_val > c_val else c_val
+            
+    r = seq_len - 1
+    c = sub_len - 1
+    lcs = ''
+
+    while r > 0 and c > 0:
+        r_bk = r - 1
+        c_bk = c - 1
+
+        if seq[r_bk] == sub[c_bk]:
+            lcs += seq[r_bk]
+            r = r_bk
+            c = c_bk
+        elif table[r_bk][c] > table[r][c_bk]:
+            r = r_bk
+        else:
+            c = c_bk
+
+    lcs = lcs[::-1]
+
+    return len(lcs)
