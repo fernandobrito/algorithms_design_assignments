16a85d55c1b8949d4c67e5eb26a620b9a13ad645
Jaelson <jaelsonjunior99@gmail.com>
2016-10-10 17:43:45 -0300

    Update binomial coefficient to use helper to initialize tables

--- dynamic_greedy_lib/dynamic/binomial_coefficient.py
+++ dynamic_greedy_lib/dynamic/binomial_coefficient.py
@@ -1,3 +1,5 @@
+from utils import table as table_utils
+
 """
     Author: Jaelson Carvalho - 11427671
     Python 3.4.3
@@ -24,25 +26,20 @@ def solve(input):
 
 def _binomial_coefficient(n, k):
     if (n >= k) and (k >= 0):
-        """
-        Create pascal triangle
-        """
-        c = [0]*(k+1)
 
-        """
-        Set the first position with 1
-        """
+        # Create a table to store values that are used to solve shortest problems
+        c = table_utils.initialize(1, k + 1, 0)[0]
+
+        # Set the first position with 1
         c[0] = 1
 
         for i in range(1, n+1):
 
-          """
-          Calculate the current row of pascal triangle using the previous row
-          """
-          j = min(i, k)
-          while j > 0:
-            c[j] = c[j] + c[j-1]
-            j -= 1
+            # Calculate the current row of pascal triangle using the previous row
+            j = min(i, k)
+            while j > 0:
+                c[j] = c[j] + c[j-1]
+                j -= 1
 
         return c[k]
 
