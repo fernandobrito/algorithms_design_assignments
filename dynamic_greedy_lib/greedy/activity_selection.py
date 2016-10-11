d320c327eca13dede7ccfac0af1f086c796cbcbb
Jaelson <jaelsonjunior99@gmail.com>
2016-10-11 13:20:20 -0300

    Create activity selection function

---
+++ dynamic_greedy_lib/greedy/activity_selection.py
@@ -0,0 +1,26 @@
+"""
+    Author: Jaelson Carvalho - 11427671
+    Python 3.4.3
+    Usage: python activity_selection.py
+    Resources:
+        http://www.geeksforgeeks.org/greedy-algorithms-set-1-activity-selection-problem/
+
+    Description:
+
+    Complexity:
+
+    Application:
+"""
+
+
+def solve(input):
+    """
+    Input : ---
+    Output : ---
+    """
+
+    return _activity_selection(input[0], input[1])
+
+
+def _activity_selection(starts, finishes):
+    return [0, 1, 3, 4]
