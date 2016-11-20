a0a943137725861a9dad14b0a36f491cc84a1325
Fernando Brito <email@fernandobrito.com>
2016-11-20 20:08:21 -0300

    Update agent to run multiple files

--- dynamic_greedy_lib/metaheuristics/tests/test.py
+++ dynamic_greedy_lib/metaheuristics/tests/test.py
@@ -4,24 +4,26 @@ from metaheuristics.tests.agent import Agent, AgentLogger
 from metaheuristics.techniques.simulated_annealing import SimulatedAnnealing
 from metaheuristics.techniques.genetic_algorithm import GeneticAlgorithm
 
-ITERATIONS_PER_FILE = 3
 HEURISTIC_CLASSES = [SimulatedAnnealing, GeneticAlgorithm]
+INPUT_FILES = ['mknap1_1.txt', 'mknap1_2.txt', 'mknap1_3.txt',
+               'mknap1_4.txt', 'mknap1_5.txt', 'mknap1_6.txt', 'mknap1_7.txt']
+ITERATIONS_PER_FILE = 3
 
 def run_test():
     AgentLogger.clear_log()
 
     for heuristic_class in HEURISTIC_CLASSES:
-        for index in range(ITERATIONS_PER_FILE):
-            filename = 'mknap1_2.txt'
-            file = open(filename, 'r')
+        for filename in INPUT_FILES:
+            for index in range(ITERATIONS_PER_FILE):
+                file = open(filename, 'r')
 
-            logger = AgentLogger(filename, str(index))
+                logger = AgentLogger(filename, str(index))
 
-            agent = Agent(heuristic_class(), file, logger)
-            agent.execute()
+                agent = Agent(heuristic_class(), file, logger)
+                agent.execute()
 
-            print("Best solution:")
-            pprint(vars(agent.best_solution()))
+                print("Best solution:")
+                pprint(vars(agent.best_solution()))
 
 
 if __name__ == "__main__":
