d6b90375a3974d4d744046588b6cd7d25e1aa851
Jaelson <jaelsonjunior99@gmail.com>
2016-11-20 14:35:48 -0300

    Add build_seed_data function to Genetic ALgorithm

--- dynamic_greedy_lib/metaheuristics/techniques/genetic_algorithmm.py
+++ dynamic_greedy_lib/metaheuristics/techniques/genetic_algorithmm.py
@@ -4,8 +4,23 @@
 from metaheuristics.techniques.metaheuristic import MetaHeuristic
 
 class GeneticAlgorithm(MetaHeuristic):
-    def __init__(self):
+    def __init__(self, seed_data, constraint_limits):
         super().__init__()
+
+        def build_seed_data(raw_data):
+            """Create a items set in a list representation, where each item is a list and its respective
+                profit is the last position
+            """
+            seed_data = []
+            for i in range(len(raw_data)):
+                data = list(raw_data[i].constraints)
+                data.append(raw_data[i].profit)
+                seed_data.append(data)
+
+            return seed_data
+
+        self.seed_data = build_seed_data(seed_data)
+        self.constraint_limits = constraint_limits
 
     def execute_once(self, knapsack):
 
@@ -15,6 +30,7 @@ class GeneticAlgorithm(MetaHeuristic):
         print ("===> Genetic Algorithm Living")
         print ("===> Best  solution: {", self.best_solution, "}")
         print ("===> Worst  solution: {", self.worst_solution, "}")
+        print ("Seed data: ", self.seed_data)
 
         return knapsack
 
